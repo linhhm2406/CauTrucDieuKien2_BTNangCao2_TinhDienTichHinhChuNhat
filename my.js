87bdd81a1fa7d1b12c7127e749c8f1ed9f49892a
linhhm24 <linh.hm24@gmail.com>
2020-02-24 22:20:09 +0700

    add file

---
+++ my.js
@@ -0,0 +1,5 @@
+let canha=parseInt(prompt('Nhap vao do dai canh a'));
+let canhb=parseInt(prompt('Nhap vao do dai canh b'));
+
+let area=(canha*canhb);
+alert('Ket qua dien tich hinh chu nhat la '+area);
